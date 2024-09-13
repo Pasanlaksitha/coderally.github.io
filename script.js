3dd144e94c717ca9539f3e575385224e654ed870
Pasan Laksitha <29460924+Pasanlaksitha@users.noreply.github.com>
2024-09-13 22:33:46 +0530

    style: Adjust clock width to be flexible and centered feat: Add .gitignore file to ignore IntelliJ IDEA files (main)

--- script.js
+++ script.js
@@ -1,3 +1,53 @@
+// window.addEventListener('load', (event) => {
+//   let clock;
+//
+//   // Grab the current date
+//   let currentDate = new Date();
+//
+//   // Target future date/24 hour time/Timezone
+//   let targetDate = moment.tz("2024-10-30 23:59", "Australia/Sydney");
+//
+//   // Calculate the difference in seconds between the future and current date
+//   let diff = targetDate / 1000 - currentDate.getTime() / 1000;
+//
+//   if (diff <= 0) {
+//     // If remaining countdown is 0
+//     clock = (".clock").FlipClock(0, {
+//       clockFace: "DailyCounter",
+//       countdown: true,
+//       autostart: false
+//     });
+//     console.log("Date has already passed!")
+//
+//   } else {
+//     // Run countdown timer
+//     clock = $(".clock").FlipClock(diff, {
+//       clockFace: "DailyCounter",
+//       countdown: true,
+//       callbacks: {
+//         stop: function() {
+//           console.log("Timer has ended!")
+//         }
+//       }
+//     });
+//
+//     // Check when timer reaches 0, then stop at 0
+//     setTimeout(function() {
+//       checktime();
+//     }, 1000);
+//
+//     function checktime() {
+//       t = clock.getTime();
+//       if (t <= 0) {
+//         clock.setTime(0);
+//       }
+//       setTimeout(function() {
+//         checktime();
+//       }, 1000);
+//     }
+//   }
+// });
+
 window.addEventListener('load', (event) => {
   let clock;
 
@@ -5,39 +55,39 @@ window.addEventListener('load', (event) => {
   let currentDate = new Date();
 
   // Target future date/24 hour time/Timezone
-  let targetDate = moment.tz("2024-10-30 23:59", "Australia/Sydney");
+  let targetDate = moment.tz("2024-9-15 08:30", "Australia/Sydney");
 
   // Calculate the difference in seconds between the future and current date
   let diff = targetDate / 1000 - currentDate.getTime() / 1000;
 
   if (diff <= 0) {
-    // If remaining countdown is 0
-    clock = (".clock").FlipClock(0, {
-      clockFace: "DailyCounter",
+    // If remaining countdown is 0, initialize the clock at 0
+    clock = $(".clock").FlipClock(0, {
+      clockFace: "HourlyCounter", // Changed from DailyCounter to HourlyCounter
       countdown: true,
       autostart: false
     });
-    console.log("Date has already passed!")
-    
+    console.log("Date has already passed!");
+
   } else {
     // Run countdown timer
     clock = $(".clock").FlipClock(diff, {
-      clockFace: "DailyCounter",
+      clockFace: "HourlyCounter", // Changed from DailyCounter to HourlyCounter
       countdown: true,
       callbacks: {
         stop: function() {
-          console.log("Timer has ended!")
+          console.log("Timer has ended!");
         }
       }
     });
-    
+
     // Check when timer reaches 0, then stop at 0
     setTimeout(function() {
       checktime();
     }, 1000);
-    
+
     function checktime() {
-      t = clock.getTime();
+      let t = clock.getTime();
       if (t <= 0) {
         clock.setTime(0);
       }
